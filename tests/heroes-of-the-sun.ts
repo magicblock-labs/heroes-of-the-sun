039736215ccc135c6fe11f35e3b4fc1bf7ea9091
Nick Balyanitsa <nick@sekai.gg>
2024-08-04 21:49:24 +0100

    extracted test script from program logic

--- tests/heroes-of-the-sun.ts
+++ tests/heroes-of-the-sun.ts
@@ -1,143 +1,23 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { PublicKey } from "@solana/web3.js";
-import {
-  InitializeNewWorld,
-  AddEntity,
-  InitializeComponent,
-  ApplySystem,
-} from "@magicblock-labs/bolt-sdk"
 import { assert, expect } from "chai";
-import { Settlement } from "../target/types/settlement";
-import { Time } from "../target/types/time";
-import { Build } from "../target/types/build";
-import { Labour } from "../target/types/labour";
-
-
-type BuildArgs = {
-  x: number, y: number, id: number
-}
-
-type AllocateLabourArgs = {
-  building: number, labour: number
-}
-
-type WaitArgs = {
-  days: number
-}
-
+import { SettlementWrapper } from "./settlement.wrapper";
 
 describe("HeroesOfTheSun", () => {
-  // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.env();
-  anchor.setProvider(provider);
 
-  // Constants used to test the program.
-  let worldPda: PublicKey;
-  let entityPda: PublicKey;
-  let componentPda: PublicKey;
+  const settlement = new SettlementWrapper();
 
-  const settlementComponent = anchor.workspace.Settlement as Program<Settlement>;
-  const timeSystem = anchor.workspace.Time as Program<Time>;
-  const buildSystem = anchor.workspace.Build as Program<Build>;
-  const labourSystem = anchor.workspace.Labour as Program<Labour>;
-
-
-  const build = async (args: BuildArgs) => {
-    // Run the build system
-    const applySystem = await ApplySystem({
-      authority: provider.wallet.publicKey,
-      systemId: buildSystem.programId,
-      entities: [{
-        entity: entityPda,
-        components: [{ componentId: settlementComponent.programId }],
-      }],
-      args
-    });
-    const txSign = await provider.sendAndConfirm(applySystem.transaction);
-    console.log(`build tx: ${txSign}`);
-
-    return await settlementComponent.account.settlement.fetch(componentPda);
-  }
-
-  const assignLabour = async (args: AllocateLabourArgs) => {
-    const applySystem = await ApplySystem({
-      authority: provider.wallet.publicKey,
-      systemId: labourSystem.programId,
-      entities: [{
-        entity: entityPda,
-        components: [{ componentId: settlementComponent.programId }],
-      }],
-      args: {
-        labour: 0,
-        building: 0
-      }
-    }
-    );
-    const txSign = await provider.sendAndConfirm(applySystem.transaction);
-    return await settlementComponent.account.settlement.fetch(componentPda);
-  }
-
-  const wait = async (args: WaitArgs) => {
-
-    // Run the movement system
-    const applySystem = await ApplySystem({
-      authority: provider.wallet.publicKey,
-      systemId: timeSystem.programId,
-      entities: [{
-        entity: entityPda,
-        components: [{ componentId: settlementComponent.programId }],
-      }],
-      args
-    }
-    );
-    const txSign = await provider.sendAndConfirm(applySystem.transaction);
-    console.log(`Waited a day: ${txSign}`);
-
-    return await settlementComponent.account.settlement.fetch(componentPda);
-
-  }
-
-  it("Initializes a NewWorld", async () => {
-    const initNewWorld = await InitializeNewWorld({
-      payer: provider.wallet.publicKey,
-      connection: provider.connection,
-    });
-    const txSign = await provider.sendAndConfirm(initNewWorld.transaction);
-    worldPda = initNewWorld.worldPda;
-    console.log(`Initialized a new world (ID=${worldPda}). Initialization signature: ${txSign}`);
-  });
-
-  it("Adds an entity", async () => {
-    const addEntity = await AddEntity({
-      payer: provider.wallet.publicKey,
-      world: worldPda,
-      connection: provider.connection,
-    });
-    const txSign = await provider.sendAndConfirm(addEntity.transaction);
-    entityPda = addEntity.entityPda;
-    console.log(`Initialized a new Entity (PDA=${addEntity.entityPda}). Initialization signature: ${txSign}`);
-  });
-
-  it("Creates a settlement", async () => {
-    const initializeComponent = await InitializeComponent({
-      payer: provider.wallet.publicKey,
-      entity: entityPda,
-      componentId: settlementComponent.programId,
-    });
-    const txSign = await provider.sendAndConfirm(initializeComponent.transaction);
-    componentPda = initializeComponent.componentPda;
-    console.log(`Initialized the grid component. Initialization signature: ${txSign}`);
+  it("Initializes", async () => {
+    const state = await settlement.init();
+    expect(state.buildings.length).to.eq(0);
   });
 
   it("Adds a building", async () => {
-    const state = await build({ x: 1, y: 1, id: 1 });
+    const state = await settlement.build({ x: 1, y: 1, id: 1 });
     expect(state.buildings.length).to.gt(0);
   });
 
   it("Prevents overlap", async () => {
     try {
-      await build({ x: 1, y: 1, id: 1 });
+      await settlement.build({ x: 1, y: 1, id: 1 });
       assert(false, "should've failed but didn't ")
     } catch (_err) {
       //why cant in just get the error from the _err type??? error types are not exposed :/
@@ -146,13 +26,13 @@ describe("HeroesOfTheSun", () => {
   });
 
   it("Allocates Labour", async () => {
-    const state = await assignLabour({ labour: 0, building: 0 });
+    const state = await settlement.assignLabour({ labour: 0, building: 0 });
     console.log(state);
   });
 
   it("Waits a day", async () => {
     // Check that the system has been applied and x is > 0
-    const state = await wait({ days: 5 });
+    const state = await settlement.wait({ days: 5 });
     console.log(state);
   });
 
---
+++ tests/settlement.wrapper.ts
@@ -0,0 +1,137 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { PublicKey } from "@solana/web3.js";
+import {
+  InitializeNewWorld,
+  AddEntity,
+  InitializeComponent,
+  ApplySystem,
+} from "@magicblock-labs/bolt-sdk"
+import { Settlement } from "../target/types/settlement";
+import { Time } from "../target/types/time";
+import { Build } from "../target/types/build";
+import { Labour } from "../target/types/labour";
+
+
+export type BuildArgs = {
+  x: number, y: number, id: number
+}
+
+export type AllocateLabourArgs = {
+  building: number, labour: number
+}
+
+export type WaitArgs = {
+  days: number
+}
+
+
+export class SettlementWrapper {
+
+  provider: anchor.AnchorProvider;
+
+  worldPda: PublicKey;
+  entityPda: PublicKey;
+  componentPda: PublicKey;
+
+
+  settlementComponent: Program<Settlement>;
+  timeSystem: Program<Time>;
+  buildSystem: Program<Build>;
+  labourSystem: Program<Labour>;
+
+  async init() {
+    this.provider = anchor.AnchorProvider.env();
+    anchor.setProvider(this.provider);
+
+    this.settlementComponent = anchor.workspace.Settlement as Program<Settlement>;
+    this.timeSystem = anchor.workspace.Time as Program<Time>;
+    this.buildSystem = anchor.workspace.Build as Program<Build>;
+    this.labourSystem = anchor.workspace.Labour as Program<Labour>;
+
+
+    const initNewWorld = await InitializeNewWorld({
+      payer: this.provider.wallet.publicKey,
+      connection: this.provider.connection,
+    });
+    let txSign = await this.provider.sendAndConfirm(initNewWorld.transaction);
+    this.worldPda = initNewWorld.worldPda;
+    console.log(`Initialized a new world (ID=${this.worldPda}). Initialization signature: ${txSign}`);
+
+    const addEntity = await AddEntity({
+      payer: this.provider.wallet.publicKey,
+      world: this.worldPda,
+      connection: this.provider.connection,
+    });
+    txSign = await this.provider.sendAndConfirm(addEntity.transaction);
+    this.entityPda = addEntity.entityPda;
+    console.log(`Initialized a new Entity (PDA=${addEntity.entityPda}). Initialization signature: ${txSign}`);
+
+    const initializeComponent = await InitializeComponent({
+      payer: this.provider.wallet.publicKey,
+      entity: this.entityPda,
+      componentId: this.settlementComponent.programId,
+    });
+    txSign = await this.provider.sendAndConfirm(initializeComponent.transaction);
+    this.componentPda = initializeComponent.componentPda;
+    console.log(`Initialized the settlement component. Initialization signature: ${txSign}`);
+
+    return await this.settlementComponent.account.settlement.fetch(this.componentPda);
+
+  }
+
+  async build(args: BuildArgs) {
+    // Run the build system
+    const applySystem = await ApplySystem({
+      authority: this.provider.wallet.publicKey,
+      systemId: this.buildSystem.programId,
+      entities: [{
+        entity: this.entityPda,
+        components: [{ componentId: this.settlementComponent.programId }],
+      }],
+      args
+    });
+    const txSign = await this.provider.sendAndConfirm(applySystem.transaction);
+    console.log(`build tx: ${txSign}`);
+
+    return await this.settlementComponent.account.settlement.fetch(this.componentPda);
+  }
+
+  async assignLabour(args: AllocateLabourArgs) {
+    const applySystem = await ApplySystem({
+      authority: this.provider.wallet.publicKey,
+      systemId: this.labourSystem.programId,
+      entities: [{
+        entity: this.entityPda,
+        components: [{ componentId: this.settlementComponent.programId }],
+      }],
+      args: {
+        labour: 0,
+        building: 0
+      }
+    }
+    );
+    const txSign = await this.provider.sendAndConfirm(applySystem.transaction);
+    return await this.settlementComponent.account.settlement.fetch(this.componentPda);
+  }
+
+  async wait(args: WaitArgs) {
+
+    // Run the movement system
+    const applySystem = await ApplySystem({
+      authority: this.provider.wallet.publicKey,
+      systemId: this.timeSystem.programId,
+      entities: [{
+        entity: this.entityPda,
+        components: [{ componentId: this.settlementComponent.programId }],
+      }],
+      args
+    }
+    );
+    const txSign = await this.provider.sendAndConfirm(applySystem.transaction);
+    console.log(`Waited a day: ${txSign}`);
+
+    return await this.settlementComponent.account.settlement.fetch(this.componentPda);
+
+  }
+};
