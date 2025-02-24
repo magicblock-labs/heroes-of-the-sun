9900ae8ef4911fc37a29f2c596aeeba892f8e49d
Nick Balyanitsa <nick@sekai.gg>
2025-02-24 17:30:42 +0000

    fixed settlement recreation on system apply

--- app/Assets/Scripts/View/Building/RenderGroundTiles.cs
+++ app/Assets/Scripts/View/Building/RenderGroundTiles.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Model;
 using Unity.AI.Navigation;
 using UnityEngine;
@@ -14,41 +15,58 @@ namespace View.Building
         [SerializeField] private GameObject surroundingTile;
         [SerializeField] private float scaleFactor = 0.95f;
 
+        private byte[,] _renderedData;
+        private Dictionary<Vector2Int, GameObject> _renderedTiles = new();
+
         public void SetData(global::Settlement.Accounts.Settlement value)
         {
-            foreach (Transform child in transform)
-                Destroy(child.gameObject);
-
             var occupiedData = _config.GetCellsData(value);
+            var rebuildNavmesh = false;
 
             for (var i = -2; i < occupiedData.GetLength(0) + 2; i++)
             for (var j = -2; j < occupiedData.GetLength(1) + 2; j++)
             {
-                GameObject prefab;
+                var posVector = new Vector2Int(i, j);
 
-                if (i < 0 || j < 0 || i >= occupiedData.GetLength(0) ||
-                    j >= occupiedData.GetLength(1))
-                    prefab = surroundingTile;
-                else if (occupiedData[i, j] == 0)
-                    prefab = tilePrefab;
+                var isSurroundingTile =
+                    i < 0 || j < 0 || i >= occupiedData.GetLength(0) || j >= occupiedData.GetLength(1);
+
+                if (isSurroundingTile)
+                {
+                    if (!_renderedTiles.ContainsKey(posVector))
+                        _renderedTiles[posVector] = CreatePrefab(surroundingTile, i, j);
+                }
                 else
-                    prefab = occupiedTilePrefab;
+                {
+                    if (_renderedData != null && _renderedData[i, j] == occupiedData[i, j])
+                        continue;
 
-                CreatePrefab(prefab, i, j);
+                    rebuildNavmesh = true;
+
+                    if (_renderedTiles.TryGetValue(posVector, out var tile))
+                        Destroy(tile);
+
+                    _renderedTiles[posVector] =
+                        CreatePrefab(occupiedData[i, j] == 0 ? tilePrefab : occupiedTilePrefab, i, j);
+                }
             }
 
             var navMeshSurface = GetComponent<NavMeshSurface>();
-            if (navMeshSurface)
+            if (navMeshSurface != null && rebuildNavmesh)
                 navMeshSurface.BuildNavMesh();
+
+            _renderedData = occupiedData;
         }
 
 
-        private void CreatePrefab(GameObject prefab, int i, int j)
+        private GameObject CreatePrefab(GameObject prefab, int i, int j)
         {
             var obj = Instantiate(prefab, transform, true);
 
             obj.transform.localPosition = ConfigModel.GetWorldCellPosition(i, j);
             obj.transform.localScale *= ConfigModel.CellSize * scaleFactor;
+
+            return obj;
         }
     }
 }
